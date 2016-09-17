4298d501a4cb33713ec81db99ad74c2d0023d238
colbyschulz <colbyschulz@gmail.com>
2016-09-17 14:53:05 -0400

    toggle button

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById('cat-alert');
   button.addEventListener('click', function() {
-
-    chrome.alarms.create("cat facts", {periodInMinutes: 0.1})
-    
-
+    if (button.value === "go") {
+      chrome.alarms.create("cat facts", {periodInMinutes: 0.05})
+      button.value = "stop"
+    }
+    else {
+      chrome.alarms.clear("cat facts")
+      button.value = "go"
+    }
   });
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+  alert("It works!")
 });
