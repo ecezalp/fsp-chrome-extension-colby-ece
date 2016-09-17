4fc0b9597812acfc540564b8640512dddf53348f
colbyschulz <colbyschulz@gmail.com>
2016-09-17 14:12:48 -0400

    added alarm permission

--- test.js
+++ test.js
@@ -2,14 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById('cat-alert');
   button.addEventListener('click', function() {
 
-    function myAlert(message) {
-      alert(message)
-    }
-
-    function alerter(){
-      setTimeout(myAlert("hello"), 3000);
-    }
-    alerter()
+    chrome.alarms.create("cat facts", {periodInMinutes: 0.1})
+    
 
   });
 });
