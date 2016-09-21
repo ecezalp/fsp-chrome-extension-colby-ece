cad555949fca1fa8605af6168febeedc60c6efaf
colbyschulz <colbyschulz@gmail.com>
2016-09-21 16:35:12 -0400

    added mute button and logic

--- cat_fact.js
+++ cat_fact.js
@@ -1,38 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var testButton = $('#test-alert');
+  var factButton = $('#facts');
+  var muteButton = $('#mute')
   var obj = $.get('http://catfacts-api.appspot.com/api/facts')
   var audio = new Audio()
   audio.src = "assets/audio/meow.mp3"
 
-  localStorage.getItem("button") ? testButton.val(localStorage.getItem("button")) : localStorage.setItem("button", "I thirst for knowledge!")
+  // Set local storage and button values
+  localStorage.getItem("button") ? factButton.val(localStorage.getItem("button")) : localStorage.setItem("button", "I thirst for knowledge!")
+  localStorage.getItem("muteButton") ? muteButton.val(localStorage.getItem("muteButton")) : localStorage.setItem("muteButton", "mute")
 
-  testButton.click(function() {
+  // mute listener
+  muteButton.click(function() {
+    toggleStorageAndMute()
+  })
 
+  // fact listener
+  factButton.click(function() {
     if (localStorage.getItem("button") === "I thirst for knowledge!") {
-      chrome.alarms.create("cat facts", {periodInMinutes: 15})
-      toggleStorageAndButton()
+      chrome.alarms.create("cat facts", {periodInMinutes: .08})
+      toggleStorageAndFacts()
     }
-
     else if (localStorage.getItem("button") === "Make it stop!") {
     chrome.alarms.clear("cat facts")
-    toggleStorageAndButton()
+    toggleStorageAndFacts()
     }
   });
 
-  function toggleStorageAndButton () {
-    if (localStorage.getItem("button") === "I thirst for knowledge!") {
-      localStorage.setItem("button", "Make it stop!")
-      testButton.attr("value", "Make it stop!")
+
+  function toggleStorageAndMute() {
+    if (localStorage.getItem("muteButton") === "mute") {
+      localStorage.setItem("muteButton", "meow!")
+      muteButton.attr("value", "meow!")
     }
-    else if (localStorage.getItem("button") === "Make it stop!") {
-      localStorage.setItem("button", "I thirst for knowledge!")
-      testButton.attr("value", "I thirst for knowledge!")
+    else if (localStorage.getItem("muteButton") === "meow!") {
+      localStorage.setItem("muteButton", "mute")
+      muteButton.attr("value", "mute")
     }
   }
 
+  function toggleStorageAndFacts() {
+    if (localStorage.getItem("button") === "I thirst for knowledge!") {
+      localStorage.setItem("button", "Make it stop!")
+      factButton.attr("value", "Make it stop!")
+    }
+    else if (localStorage.getItem("button") === "Make it stop!") {
+      localStorage.setItem("button", "I thirst for knowledge!")
+      factButton.attr("value", "I thirst for knowledge!")
+    }
+  }
+  //Listens for alarms and retrieves fact
   chrome.alarms.onAlarm.addListener(function(alarm){
-    audio.play();
+    if (localStorage.getItem("muteButton") === "mute") {
+      audio.play();
+    }
     alert(catFactAlertMessages())
     refreshObject();
   });
